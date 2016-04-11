c8f22a4102e36eaf63e5202b5fe80f362d5672dc
Thiago Durante Pires <thdurante@gmail.com>
2016-04-11 09:01:13 -0300

    finished refs to components

---
+++ hello-react/public/scripts/example.js
@@ -0,0 +1,27 @@
+var MyComponent = React.createClass({
+    handleClick: function() {
+        // Explicitly focus the text input using the raw DOM API.
+        if (this.myTextInput !== null) {
+            this.myTextInput.focus();
+        }
+    },
+    render: function() {
+        // The ref attribute is a callback that saves a reference to the
+        // component to this.myTextInput when the component is mounted.
+        return (
+            <div>
+                <input type="text" ref={(ref) => this.myTextInput = ref} />
+                <input
+                    type="button"
+                    value="Focus the text input"
+                    onClick={this.handleClick}
+                />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <MyComponent />,
+    document.getElementById('example')
+);
